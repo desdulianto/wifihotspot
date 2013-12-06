7be5f34f09855d3e822119eae2cdf6a10830bd35
Des Dulianto <desdulianto@gmail.com>
2013-12-06 14:46:16 +0700

    refactor

--- app/vouchers/views.py
+++ app/vouchers/views.py
@@ -11,6 +11,34 @@ import json
 
 blueprint = Blueprint('vouchers', __name__)
 
+def generateVoucher():
+    symbols = app.config.get('VOUCHER_SYMBOLS', string.ascii_letters +
+            string.digits)
+    length = app.config.get('VOUCHER_LENGTH', 8)
+
+    while True:
+        voucher = ''.join([symbols[random.randint(0, len(symbols)-1)] for x in
+            xrange(length)])
+        if models.RadCheck.query.filter_by(username=voucher).first() == None:
+            break
+    return voucher
+
+
+def addToRadius(voucher, name, phone, groupname):
+    radcheck = models.RadCheck(username=voucher, attribute='Cleartext-Password',
+            op=':=', value=voucher, name=name, phone=phone)
+    radgroup = models.RadUserGroup(username=voucher, groupname=groupname,
+            priority=1)
+    db.session.add(radcheck)
+    db.session.add(radgroup)
+    db.session.commit()
+
+
+def sendSMS(phone, text):
+    redis.rpush(app.config['SMS_KEY'], json.dumps(dict(Number=phone, 
+        Text=text)))
+
+
 @blueprint.route('/', endpoint='index')
 def index():
     return render_template('index.html')
@@ -18,20 +46,11 @@ def index():
 
 @blueprint.route('/<name>/<phone>', methods=['POST'], endpoint='add_voucher_by_phone')
 def add_voucher_by_phone(name, phone):
-    symbols = app.config.get('VOUCHER_SYMBOLS', string.ascii_letters +
-            string.digits)
-    length = app.config.get('VOUCHER_LENGTH', 8)
-
-    voucher = ''.join([symbols[random.randint(0, len(symbols)-1)] for x in
-        xrange(length)])
+    voucher = generateVoucher()
 
     # save to radius db
-    radcheck = models.RadCheck(username=voucher, attribute='Cleartext-Password',
-            op=':=', value=voucher)
-    db.session.add(radcheck)
-    db.session.commit()
+    addToRadius(voucher, name, phone, 'hotspot')
 
     # send to sms queue
-    redis.rpush(app.config['SMS_KEY'], json.dumps(dict(Number=phone, 
-        Text='Nomor voucher Wifi HotSpot: %s' % voucher)))
+    sendSMS(phone, text='Nomor voucher Wifi HotSpot: %s' % voucher)
     return jsonify(status='OK', phone=phone, voucher=voucher)
