ef1142ca7dfe984e0f7def968ea4711f3fc9f7cb
Des Dulianto <desdulianto@gmail.com>
2013-12-07 17:02:33 +0700

    add user management views

--- app/users/forms.py
+++ app/users/forms.py
@@ -2,6 +2,8 @@ from flask.ext.wtf import Form
 from wtforms import TextField, PasswordField
 from wtforms.validators import DataRequired, ValidationError
 
+import models
+
 
 class LoginForm(Form):
     name = TextField('Nama User', validators=[DataRequired()])
@@ -11,6 +13,31 @@ class LoginForm(Form):
 class ChangePasswordForm(Form):
     password = PasswordField('Password')
     password_verify = PasswordField('Verifikasi Password')
+
+    def validate_password(form, field):
+        password = form.password.data
+
+        if len(password) < 8:
+            raise ValidationError('Password min. 8 karakter!')
+
+    def validate_password_verify(form, field):
+        password = form.password.data
+        password_verify = field.data
+
+        if password != password_verify:
+            raise ValidationError('Password tidak sama!')
+
+
+class UserForm(Form):
+    name = TextField('Nama User')
+    password = PasswordField('Password')
+    password_verify = PasswordField('Verifikasi Password')
+
+    def validate_name(form, field):
+        name = field.data.strip()
+
+        if models.User.query.filter_by(name=name).first() != None:
+            raise ValidationError('User sudah terdaftar!')
 
     def validate_password(form, field):
         password = form.password.data
--- app/users/views.py
+++ app/users/views.py
@@ -1,6 +1,9 @@
 from flask import Blueprint
 from flask import request, url_for, redirect, render_template, g, flash
 from flask.ext.login import login_user, logout_user, login_required
+from flask.ext.paginate import Pagination
+
+from werkzeug.exceptions import Forbidden
 
 from app import app, login_manager, db
 
@@ -41,6 +44,29 @@ def logout():
     return redirect(url_for('login'))
 
 
+@blueprint.route('/', methods=['GET'], endpoint='index')
+@login_required
+def index():
+    per_page = 20
+    try:
+        page = int(request.args.get('page', 1))
+    except ValueError:
+        page = 1
+
+    users = models.User.query.order_by(models.User.name)
+
+    pagination = Pagination(page=page, per_page=per_page, bs_version=3,
+            total=users.count(), record_name='users', search=False)
+    return render_template('list.html',
+            items=users.limit(per_page).offset((page-1)*per_page).all(),
+            columns=[dict(title='Nama User', field='name'),
+                     dict(title='Active', field='active')],
+            title='Daftar User',
+            create_url='.new',
+            edit_url='.edit',
+            void_url='.delete')
+
+
 @blueprint.route('/account', methods=['GET', 'POST'], endpoint='account')
 @login_required
 def user_account():
@@ -55,5 +81,60 @@ def user_account():
 
         flash('Password telah diupdate!', 'success')
         return redirect(next_page)
-    return render_template('form_complex.html', title='Change Password',
+    return render_template('form_complex.html', 
+            title='Ubah Password {name}'.format(name=user.name),
             fields=['password', 'password_verify'], form=form)
+
+
+@blueprint.route('/<int:id>', methods=['GET', 'POST'], endpoint='edit')
+@login_required
+def edit_user(id):
+    if g.user.name != 'admin':
+        raise Forbidden('Hanya admin yang dapat mengubah password')
+    user = models.User.query.get_or_404(id)
+
+    form = forms.ChangePasswordForm()
+    if form.validate_on_submit():
+        user.password_change(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+
+        flash('Password {name} telah diupdate!'.format(name=user.name),
+                'success')
+        return redirect(url_for('.index'))
+    return render_template('form_complex.html', 
+            title='Ubah Password {name}'.format(name=user.name),
+            fields=['password', 'password_verify'], form=form)
+
+
+@blueprint.route('/new', methods=['GET', 'POST'], endpoint='new')
+@login_required
+def new_user():
+    form = forms.UserForm(formdata=request.form)
+    if form.validate_on_submit():
+        user = models.User(name=form.name.data.strip(),
+                password=models.User.password_hash(form.password.data))
+
+        db.session.add(user)
+        db.session.commit()
+        flash('User {name} telah ditambahkan!'.format(name=user.name),
+            'success')
+        return redirect(url_for('.index'))
+    return render_template('form_complex.html', form=form, 
+            title='User Baru',
+            fields=['name', 'password', 'password_verify'])
+
+
+@blueprint.route('/delete/<int:id>', methods=['GET', 'POST'],
+        endpoint='delete')
+@login_required
+def delete_user(id):
+    user = models.User.query.get_or_404(id)
+
+    if user.name == 'admin':
+        flash('User admin tidak bisa dihapus!', 'warning')
+    else:
+        flash('User {name} telah dihapus!'.format(name=user.name), 'success')
+        db.session.delete(user)
+        db.session.commit()
+    return redirect(url_for('.index'))
